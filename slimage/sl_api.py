3c83edb61fff38bf85a40740d70ef381b04f81fb
Matthew Treinish <mtreinish@kortar.org>
2018-02-12 16:04:05 -0500

    Make create_image call functional
    This commit fixes a couple of issues with the create_image function
    found from actually trying to use it. The first issue is despite the
    python client docs indicating the os_code is an optional parameter, it
    turns out it is required. (which matches the web ui experience) The
    second is the swift username for the external uri is not your username
    but actually the project name.

--- slimage/sl_api.py
+++ slimage/sl_api.py
@@ -21,13 +21,13 @@ default_swift_url = 'https://dal05.objectstorage.softlayer.net/auth/v1.0'
 
 class SLImages(object):
     def __init__(self, swift_username, swift_auth_url=default_swift_url):
-        conf = config.get_client_settings_config_file()
+        self.conf = config.get_client_settings_config_file()
         self.swift_username = swift_username
         self.sl_client = sl.create_client_from_env()
         self.images = sl.ImageManager(self.sl_client)
         self.swift = client.Connection(authurl=swift_auth_url,
                                        user=swift_username,
-                                       key=conf['api_key'])
+                                       key=self.conf['api_key'])
 
     def get_swift_containers(self):
         _, containers = self.swift.get_account()
@@ -36,8 +36,12 @@ class SLImages(object):
     def upload_file(self, path, name, container):
         self.swift.put_object(container, name, contents=open(path, 'r'))
 
-    def create_image(self, image_name, object_name, cluster='dal05',
-                     desc=None):
-        uri = 'swift://%s@%s/slimage/%s' % (self.swift_username, cluster,
-                                            object_name)
-        self.images.import_image_from_uri(image_name, uri, note=desc)
+    def create_image(self, image_name, object_name, container='slimage',
+                     cluster='dal05', desc=None, os_type=None):
+        if os_type is None:
+            os_type = 'DEBIAN_8_64'
+        uri_user = self.swift_username.split(':')[0]
+        uri = 'swift://%s@%s/%s/%s' % (uri_user, cluster, container,
+                                       object_name)
+        self.images.import_image_from_uri(image_name, uri, os_code=os_type,
+                                          note=desc)
